490a9c59d2c72f8e9714f856b16284a3f7e347cd
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-18 17:00:06 +0900

    Create CustomOption.ts

---
+++ hison/options/CustomOption.ts
@@ -0,0 +1,11 @@
+import { dataOption } from "./dataOption";
+import { linkOption } from "./linkOption";
+import { shieldOption } from "./shieldOption";
+import { utilsOption } from "./utilsOption";
+
+export class CustomOption {
+    utils = utilsOption;
+    shield = shieldOption;
+    data = dataOption;
+    link = linkOption;
+}
