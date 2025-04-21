785102210db65040ffedddef25798066e744490d
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-21 15:23:01 +0900

    Update ApiPacth.ts

--- src/hison/link/ApiPacth.ts
+++ src/hison/link/ApiPacth.ts
@@ -113,7 +113,7 @@ export class ApiPatch<T = InterfaceDataWrapper> implements InterfaceApiPatch<T> 
      * @param {CachingModule} [cachingModule=null] - An optional caching module.
      * @throws {Error} If `serviceCmd` is not provided.
      */
-    constructor(serviceCmd: string | null = null, cachingModule: InterfaceCachingModule | null = null) {
+    constructor(serviceCmd: string = '', cachingModule: InterfaceCachingModule | null = null) {
         if (cachingModule && cachingModule.getIsCachingModule && cachingModule.getIsCachingModule()) this._cachingModule = cachingModule;
         this._eventEmitter = new EventEmitter();
         this._apiLink = new ApiLink(this._eventEmitter, this._cachingModule);
