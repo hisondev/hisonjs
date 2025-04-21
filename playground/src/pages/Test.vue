f1f01b3fe13c3f7abc9ae4b6db9f855703c640dd
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-21 15:31:07 +0900

    Create Test.vue

---
+++ playground/src/pages/Test.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>Hisonjs test</h1>
+      <HisonjsTest />
+    </div>
+</template>
+
+<script setup lang="ts">
+import HisonjsTest from '../components/HisonjsTest.vue';
+</script>
