34eeb202bae5c08dbd649e3b96c0f7906d747ef5
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-22 09:15:11 +0900

    Update main.ts

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,15 +1,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { createHison, Hison, MonthFormat } from 'hisonjs';
+import { createHison, Hison } from 'hisonjs';
 
 const app = createApp(App);
 
 const hison: Hison = createHison();
-console.log(hison);
-console.log(hison.getMonthFormat());
-hison.setMonthFormat(MonthFormat.MMMM);
-console.log(hison.getMonthFormat());
+console.log(hison.getIsPossibleOpenDevTool());
+hison.setDoDetectDevTool(() => {
+    debugger;
+});
+console.log(hison.getIsPossibleOpenDevTool());
+hison.shield.excute(hison);
 app.config.globalProperties.$hison = hison;
 
 app.use(router).mount('#app');
